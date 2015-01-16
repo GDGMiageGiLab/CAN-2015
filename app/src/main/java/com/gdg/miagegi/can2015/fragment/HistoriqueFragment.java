9a442aaed17ddc7431bfe1807e2eb5ca640b91de
Agnaramon Boris <agnaramon9228@gmail.com>
2015-01-16 22:46:03 +0000

    Historique & Stades
    Contenu ajouté

--- app/src/main/java/com/gdg/miagegi/can2015/fragment/HistoriqueFragment.java
+++ app/src/main/java/com/gdg/miagegi/can2015/fragment/HistoriqueFragment.java
@@ -5,7 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.WebView;
 
 import com.gdg.miagegi.can2015.R;
 
@@ -21,6 +21,9 @@ public class HistoriqueFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historique, container, false);
+        View view = inflater.inflate(R.layout.fragment_historique, container, false);
+        WebView webview = (WebView) view.findViewById(R.id.webviewhistorique);
+        webview.loadUrl("file:///android_res/raw/histoire.html");
+        return view;
     }
 }
